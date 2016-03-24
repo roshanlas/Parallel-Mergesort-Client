7379492f7829836cb54fd3bf15886712fe5fea38
Roshan Lasrado <roshanlasrado94@gmail.com>
2016-03-24 15:06:32 +0530

    Appearance Changes

--- app/src/main/java/com/roshan/parallelclient/HomeActivity.java
+++ app/src/main/java/com/roshan/parallelclient/HomeActivity.java
@@ -1,9 +1,7 @@
 package com.roshan.parallelclient;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +9,7 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class HomeActivity extends Activity {
 
     static TextView promptTxt;
     static ScrollView promptScroll;
@@ -63,27 +61,5 @@ public class HomeActivity extends ActionBarActivity {
                 });
             }
         }).run();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
